3faa104549f074d946debb7d59157575a1412ff5
camilobr89 <camilobr89@hotmail.com>
2022-09-09 12:30:58 -0500

    Ajuste de variables final

--- js/sesion.js
+++ js/sesion.js
@@ -31,23 +31,23 @@ function inicioSesion(){
     
     }
 
-    if (usuario == "EZambrano" && password == "1004193541") {
+    if (usuario === "EZambrano" && password === "1004193541") {
                 location.href = "../src/desarrollador1.html";
         } 
         
-    if (usuario == "DVargas" && password == "1110584386") {
+    if (usuario === "DVargas" && password === "1110584386") {
                 location.href = "../src/desarrollador2.html"
         }
 
-    if (usuario == "DOrtiz" && password == "1014658307") {
+    if (usuario === "DOrtiz" && password === "1014658307") {
                 location.href = "../src/desarrollador3.html";
         }
 
-    if (usuario == "DRojas" && password == "1094938687") {
+    if (usuario === "DRojas" && password === "1094938687") {
                 location.href = "../src/desarrollador4.html";
         }
 
-    if (usuario == "CBarrera" && password == "1022954370") {
+    if (usuario === "CBarrera" && password === "1022954370") {
                 location.href = "../src/desarrollador5.html";
         } 
         
